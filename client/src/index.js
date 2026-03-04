295666dda8bf1187eed719d4771dfe1293b93727
Pradeeps <pradeepsivasubramanian@gmail.com>
2026-03-04 22:39:52 +0530

    Remove reportWebVitals completely

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react';  // Import React for building the app
+import ReactDOM from 'react-dom/client';   // Import React and ReactDOM for rendering the app
+import './index.css'; // Import global styles
+import App from './App'; // Import the main App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
